34ed0c868272df3e8ddd862062af5e72c4a5db49
AJLud <a.ludlowww@gmail.com>
2021-05-27 14:31:33 +0100

    functionality in place ready for styling

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -4,28 +4,30 @@ import { getCategories } from "./Utils/api";
 
 const Nav = () => {
   const [categories, setCategories] = useState([]);
+  const [isLoadingCategories, setIsLoadingCategories] = useState(true);
 
   useEffect(() => {
     getCategories().then((categories) => {
+      setIsLoadingCategories(false);
       setCategories(categories);
     });
   }, []);
 
+  if (isLoadingCategories) return <p className="navbar">Loading Categories</p>;
+
   return (
-    <div>
-      <div className="Navbar">
-        <Link to="/">
-          <button className="navbar-contents">All</button>
-        </Link>
-        {categories.map((category) => {
-          return (
-            <Link key={category.slug} to={`/category/${category.slug}`}>
-              <button className="navbar-contents">{category.slug}</button>
-            </Link>
-          );
-        })}
-      </div>
-    </div>
+    <nav className="navbar">
+      <Link to="/">
+        <button className="navbar-contents">All</button>
+      </Link>
+      {categories.map((category) => {
+        return (
+          <Link key={category.slug} to={`/category/${category.slug}`}>
+            <button className="navbar-contents">{category.slug}</button>
+          </Link>
+        );
+      })}
+    </nav>
   );
 };
 
